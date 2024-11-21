1e6c6fb06dd714de5806667751b68c01bd1f58d0
Fermin Estrada Buendia <80554717+ferminestrad@users.noreply.github.com>
2024-11-21 09:40:19 -0600

    st
    WEB 2.1

---
+++ contac.js
@@ -0,0 +1,4 @@
+document.querySelector('.contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Gracias por contactarnos! Hemos recibido tu mensaje.');
+});
